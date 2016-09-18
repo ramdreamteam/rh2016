3cfffae51a0d8decaa7efa6af4ea394392a18bed
Jon Small <jonssmall@gmail.com>
2016-09-18 02:14:50 -0400

    ng route

--- public/app.js
+++ public/app.js
@@ -6,9 +6,25 @@ app.config(function($routeProvider) {
     templateUrl : 'dashboard.html',
     controller  : 'DashboardController'
   })
+  .when('/efc', {
+    templateUrl : 'expected.html',
+    controller  : 'efcController'
+  })
+  .when('/repayment', {
+    templateUrl : 'repayment.html',
+    controller  : 'repaymentController'
+  })
   .otherwise({redirectTo: '/dashboard'});
 });
 
 app.controller('DashboardController', function($scope) {
   $scope.message = 'Hello from Dashbaord';
+});
+
+app.controller('efcController', function($scope) {
+  $scope.message = 'Hello from efc';
+});
+
+app.controller('repaymentController', function($scope) {
+  $scope.message = 'Hello from repayment';
 });
