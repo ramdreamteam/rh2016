dec35b099bb6a4c0b22cbfff27062974f63520ca
Jon Small <jonssmall@gmail.com>
2016-09-18 01:30:57 -0400

    basic SAR decorating user

---
+++ capitalOneSample.js
@@ -0,0 +1,48 @@
+// http://api.reimaginebanking.com/enterprise/customers?key=824c24c68d81a8c3cfae36882a5ba1a9
+
+var customers = [
+    {
+      "_id": "56c66be5a73e492741507272",
+      "address": {
+        "city": "Union",
+        "state": "Kentucky",
+        "street_name": "Lakeview Drive",
+        "street_number": "11028",
+        "zip": "41091"
+      },
+      "first_name": "Allison",
+      "last_name": "Williams"
+    },
+    {
+      "_id": "56c66be5a73e492741507273",
+      "address": {
+        "city": "Jamestown",
+        "state": "New York",
+        "street_name": "Whitehill Circle",
+        "street_number": "1729",
+        "zip": "14701"
+      },
+      "first_name": "Jennifer",
+      "last_name": "Lawrence"
+    },
+    {
+      "_id": "56c66be5a73e492741507274",
+      "address": {
+        "city": "Lucasville",
+        "state": "Ohio",
+        "street_name": "Rapp-Hollow Road",
+        "street_number": "1102",
+        "zip": "45648"
+      },
+      "first_name": "Emilia",
+      "last_name": "Clarke"
+    },
+    {
+      "_id": "56c66be5a73e492741507275",
+      "address": {
+        "street_number": "0000"
+      },
+      "first_name": "Emily",
+      "last_name": "Kinney"
+    }
+];
